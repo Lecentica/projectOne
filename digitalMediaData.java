3ed1b7af4f2f8c3d58fd9db1acf43988564ff329
Brayden Kim <brayden.kim@proton.me>
2026-09-17 14:44:54 -0500

    inheritance

---
+++ digitalMediaData.java
@@ -0,0 +1,13 @@
+public class digitalMediaData extends productData{
+    protected String director;
+    protected String country;
+    protected String rating;
+    protected String discription;
+
+    public digitalMediaData(int i, String t, String n, String p, int r ) {
+        super(i, t, n, p, r);
+
+
+    }
+    
+}
